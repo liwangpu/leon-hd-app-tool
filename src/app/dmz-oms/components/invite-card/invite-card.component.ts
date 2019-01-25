4c42b7e6da66f63ef56a6cf7b8e9af152f662f85
leon <leon@damaozhu.com>
2019-01-25 17:51:12 +0800

    修复二维码扫描路径错误

--- src/app/dmz-oms/components/invite-card/invite-card.component.ts
+++ src/app/dmz-oms/components/invite-card/invite-card.component.ts
@@ -3,7 +3,6 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { map, skipWhile, switchMap } from 'rxjs/operators';
 import { AccountService } from '../../services/account.service';
-import { AppConfigService } from '../../../app-config.service';
 
 @Component({
   selector: 'app-invite-card',
@@ -17,7 +16,7 @@ export class InviteCardComponent implements OnInit {
   inviterCompanyIcon: string;
   inviterName: string;
   loginForm: FormGroup;
-  constructor(protected formBuilder: FormBuilder, protected route: ActivatedRoute, protected accountSrv: AccountService, protected configSrv: AppConfigService) {
+  constructor(protected formBuilder: FormBuilder, protected route: ActivatedRoute, protected accountSrv: AccountService) {
     this.loginForm = this.formBuilder.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]]
@@ -36,6 +35,7 @@ export class InviteCardComponent implements OnInit {
         // "/assets/images/morejee.png"
         this.setInviteUrl(acc.id);
       });
+
   }//ngOnInit
 
   readyToMake() {
@@ -43,8 +43,10 @@ export class InviteCardComponent implements OnInit {
   }//readyToMake
 
   setInviteUrl(userId: string) {
-    this._inviteUrl = `${this.configSrv.appConfig.server}/dmz-oms/member-invite?u=${userId}`;
-    console.log(2, this._inviteUrl);
+    if (location )
+    {
+      this._inviteUrl = `${location.protocol}//${location.host}/dmz-oms/member-invite?u=${userId}`;
+    }
   }//setInviteUrl
 
 }
