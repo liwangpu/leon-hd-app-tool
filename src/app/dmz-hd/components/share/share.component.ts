0e42309ba3adacd2321be2fe495e37a5ec29373c
leon <leon.pu@gmail.com>
2019-03-27 14:44:50 +0800

    修复分享文件页面加载出现不应该出现的提示

--- src/app/dmz-hd/components/share/share.component.ts
+++ src/app/dmz-hd/components/share/share.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { ShareService } from '../../services/share.service';
 import { ActivatedRoute } from '@angular/router';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { AppConfigService } from '../../../app-config.service';
 import { DomSanitizer, SafeResourceUrl, } from '@angular/platform-browser';
-
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-share',
@@ -18,22 +18,31 @@ export class ShareComponent implements OnInit {
   password: string;
   errpassword = false;
   errpasswordTimeInt: any;
-  showPanorama = false;
+  showDialog = false;
   panoramaUrl: SafeResourceUrl;
-  constructor(protected route: ActivatedRoute, protected shareSrv: ShareService, protected configSrv: AppConfigService, public sanitizer: DomSanitizer) { }
+  constructor(@Inject(PLATFORM_ID) platformId: Object, protected route: ActivatedRoute, protected shareSrv: ShareService, protected configSrv: AppConfigService, protected sanitizer: DomSanitizer) {
+
+    if (isPlatformBrowser(platformId)) {
+      this.route.queryParams
+        .pipe(map(parm => parm['id']))
+        .pipe(tap(id => this.shareId = id))
+        .pipe(switchMap(id => this.shareSrv.viewShare(id))).subscribe(res => {
+          this.viewPanorama(res.body['fileAssetUrl']);
+        }, err => {
+          if (err.status == 403) {
+            this.needPwd = true;
+          }
+          else {
+            //资源不存在或者过期
+            this.needPwd = false;
+          }
+          this.showDialog = true;
+        });
+    }//if
+  }//constructor
 
   ngOnInit() {
-    this.route.queryParams
-      .pipe(map(parm => parm['id']))
-      .pipe(tap(id => this.shareId = id))
-      .pipe(switchMap(id => this.shareSrv.viewShare(id))).subscribe(res => {
-        this.viewPanorama(res.body['fileAssetUrl']);
-      }, err => {
-        if (err.status == 403)
-          this.needPwd = true;
-        else
-          this.needPwd = false;
-      });
+
   }//ngOnInit
 
   view() {
@@ -55,7 +64,7 @@ export class ShareComponent implements OnInit {
   }//showErrorPwd
 
   viewPanorama(furl) {
-    this.showPanorama = true;
+    this.showDialog = false;
     this.panoramaUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`/assets/extensions/panorama/panorama.html?url=${this.configSrv.appConfig.server}${furl}`);
   }//viewPanorama
 
--- src/app/dmz-hd/services/share.service.ts
+++ src/app/dmz-hd/services/share.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AppConfigService } from '../../app-config.service';
-import { observable } from 'rxjs';
+
 
 @Injectable()
 export class ShareService {
