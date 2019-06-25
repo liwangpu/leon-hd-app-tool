98671129404d9c446e8e4af7960ce738faa0d214
leon <liwang.pu@gmail.com>
2019-06-25 16:49:03 +0800

    为挪亚家订单预览界面添加导出表格功能

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,3 +1,4 @@
 export class AppConfig {
     server: string;
+    omsToolServer: string;
 }
--- src/app/dmz-oms/components/order-viewer/order-viewer.component.ts
+++ src/app/dmz-oms/components/order-viewer/order-viewer.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/co
 import { OrderDetail } from '../../models/order-detail';
 import { OrderService } from '../../services/order.service';
 import { ActivatedRoute } from '@angular/router';
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { AppConfigService } from '../../../app-config.service';
+import { saveAs } from 'file-saver/FileSaver';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-order-viewer',
@@ -11,23 +14,25 @@ import { map, switchMap } from 'rxjs/operators';
 })
 export class OrderViewerComponent implements OnInit {
 
+  private _currentOrderId: string;
+  enableExportExcel = true;
   showFurnitureList = false;
   totalPrice: number = 0;
   orderDetails: Array<OrderDetail> = [];
   @ViewChild('a4Ct') a4Ct: ElementRef;
-  constructor(protected route: ActivatedRoute, protected orderSrv: OrderService, protected renderer2: Renderer2) {
+  constructor(protected route: ActivatedRoute, protected orderSrv: OrderService, protected renderer2: Renderer2, protected appConfigSrv: AppConfigService, protected httpClient: HttpClient) {
 
   }//constructor
 
   ngOnInit() {
     this.route.queryParams
       .pipe(map(parm => parm['order']))
+      .pipe(tap(oid => this._currentOrderId = oid))
       .pipe(switchMap(id => this.orderSrv.getById(id)))
       .subscribe(res => {
         if (!res) return;
         this.totalPrice = res.totalPrice;
         this.orderDetails = res.orderDetails ? res.orderDetails : [];
-        // console.log(1, res);
       });
   }//ngOnInit
 
@@ -64,5 +69,19 @@ export class OrderViewerComponent implements OnInit {
       this.renderer2.removeClass(it, 'actived');
     window.print();
   }//printBILL
+
+  exportExcel() {
+    if (!this.enableExportExcel) return;
+    this.enableExportExcel = false;
+    let api = encodeURIComponent(this.appConfigSrv.appConfig.server);
+    let excelUrl = `${this.appConfigSrv.appConfig.omsToolServer}/dmz/oms/norya/order2excel?orderId=${this._currentOrderId}&server=${api}`;
+    this.httpClient.get(excelUrl, { responseType: 'blob' }).subscribe(fs => {
+      saveAs(fs, 'Order Detail.xlsx');
+      this.enableExportExcel = true;
+    }, err => {
+      console.error("导出表格出现异常:", err);
+      this.enableExportExcel = true;
+    });
+  }//exportExcel
 
 }
